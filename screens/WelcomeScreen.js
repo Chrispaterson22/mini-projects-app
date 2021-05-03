f468af5e04fbf33ee036be9cabd2bb2846cbdc7b
Chris Paterson <chrispaterson22@gmail.com>
2021-05-03 16:00:06 +0100

    nav from welcome screen

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,10 +1,18 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Button } from 'react-native'
 
-const WelcomeScreen = () => {
+const WelcomeScreen = ({ props, navigation }) => {
 	return (
 		<View>
 			<Text>Welcome/Home Screen</Text>
+			<Button
+				title='Go To Projects'
+				onPress={() => navigation.navigate('ProjectsList')}
+			/>
+			<Button
+				title='Random Project'
+				onPress={() => navigation.navigate('Project')}
+			/>
 		</View>
 	)
 }
